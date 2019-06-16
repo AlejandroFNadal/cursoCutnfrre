c0739c706b2cb051d9cd51787b2e3c99b31a41f6
AnastaciusWright <32724827+AnastaciusWright@users.noreply.github.com>
2019-06-16 20:50:54 -0300

    Add files via upload

--- cursoCutnfrre/cortecontrol.c
+++ cursoCutnfrre/cortecontrol.c
@@ -1,56 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+int comparador(char s1[],char s2[])
+{
+	if(strlen(s1)!=strlen(s2))
+	{
+		return 0;
+	}
+	else
+	{
+		int n=0;
+		while(n<strlen(s1)-1)
+		{
+			if(s1[n]!=s2[n])
+				return 0;
+			n++;
+		}
+	}
+return 1;
+}
 struct clienteleido
 	{
 		int id;
-		char ciudad[30];
-		char pais[30];
+		char ciudad[50];
+		char pais[50];
 	}lineaAux;
 
-struct clienteleido retornalinea(char x[])
+struct clienteleido retornalinea(struct clienteleido nuevalinea,char x[])//recibe un string y lo distribuye en la estructura
 {
+	char aux1[7];
+	int n=0;
+	while(x[n] !=',')
+	{
+		aux1[n]=x[n];
+		n++;
+	}
+	n++;
+	nuevalinea.id=atoi(aux1);
+	int k=0;
+	while(x[n] !=',')
+        {
+                nuevalinea.ciudad[k]=x[n];
+                n++;
+		k++;
+        }
+	nuevalinea.ciudad[k]='\0';
+	k=0;
+	n++;
+        while(x[n] !='\0')
+        {
+                nuevalinea.pais[k]=x[n];
+                n++;
+                k++;
+        }
+	nuevalinea.pais[k]='\0';
+return nuevalinea;
+}
+
+void cortepais(FILE *p)
+{
+	char linea[60],aux[50];
+	struct clienteleido tempo;
+	fgets(linea,60,p);
+	tempo=retornalinea(tempo,linea);
+	strcpy(aux,tempo.pais);
+	int cntpais=0;
+	while(tempo.id != 0)//marca de fin
+	{
+		while(tempo.id != 0 && strcmp(tempo.pais,aux)==0)
+		{
+			printf("%8d\n",tempo.id);
+			fgets(linea,30,p);
+			tempo=retornalinea(tempo,linea);
+			cntpais++;
+		}
+		printf("%s %d\n",aux,cntpais);
+		cntpais=0;
+		strcpy(aux,tempo.pais);
+	}
 }
 int main()
 {
 	FILE *puntarch;
 	
-	char linea[60];
-	char aux1[6];
-	char aux2[15];
 	puntarch=fopen("corte.txt", "r");
-	printf("Corte Control \n");
-	fgets(linea,30,puntarch);
-	printf("%s",linea);
-	int n;
-	while(linea[n] !=',')
+	if(puntarch==NULL)
 	{
-		aux1[n]=linea[n];
-		n++;
+		printf("Error, archivo no encontrado");
+		exit(0);
 	}
-	n++;
-	lineaAux.id=atoi(aux1);
-	printf("%d",lineaAux.id);
-	int k=0;
-	while(linea[n] !=',')
-        {
-                lineaAux.ciudad[k]=linea[n];
-                n++;
-		k++;
-        }
-	lineaAux.ciudad[k]='\0';
-	printf("%s",lineaAux.ciudad);
-	k=0;
-	n++;
-        while(linea[n] !='\0')
-        {
-                lineaAux.pais[k]=linea[n];
-                n++;
-                k++;
-        }
-	lineaAux.pais[k]='\0';
-	printf("%s",lineaAux.pais);
+	printf("Corte Control \n");
+	cortepais(puntarch);
 	getchar();
 	fclose(puntarch);
 	return 0;
 }
+ 

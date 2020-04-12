62c3741d9fc95f5f295056fdb695725d9e19ff1d
AnastaciusWright <alexandronadal@gmail.com>
2020-04-12 12:10:15 -0300

    Prueba de gitignore

---
+++ getchar1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+  char a;
+  char b;
+  
+  printf("Ingrese el primer valor\n");
+  a=getchar();
+  printf("Ingrese el segundo valor\n");
+  b=getchar();
+
+  printf("Los dos caracteres ingresados son %c %c \n", a,b);
+  getchar();
+  return 0;
+}

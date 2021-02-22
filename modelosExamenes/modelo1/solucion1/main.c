0d459749b6c24952d17e72e3621b3b473eef9f6c
AnastaciusWright <alexandronadal@gmail.com>
2021-02-22 15:53:33 -0300

    Last changes before PC deletion

--- modelosExamenes/modelo1/solucion1/main.c
+++ modelosExamenes/modelo1/solucion1/main.c
@@ -35,6 +35,8 @@ int mes_menor_ventas;
 
 void print_mes(int val);
 void add_lista(nodo *p);
+void print_nodo(nodo *p);
+void print_fecha(fecha f);
 void print_lista();
 //puntero al primer elemento
 nodo *prim = NULL;
@@ -43,49 +45,53 @@ nodo *prim = NULL;
 int tam = 0;
 int main()
 {
-    char linea[6];
+    char linea[20];
     int pos = 0;
     //pos 0 anio, pos 1 mes, pos 2 dia, pos 3 venta
     //Lee siempre y cuando no entre vacio
     int index = 0;
     fecha nuevaFecha;
     nodo *nuevoNodo = NULL;
-    while(fgets(linea,6,stdin) != NULL)
+    while(fgets(linea,15,stdin) != NULL)
     {
-	printf("%s\n",linea);
-	//reiniciamos el evento
-	if(pos == 4)
-	    pos = 0;
-	if(pos == 0)
-	{
-	    printf("Pos 0\n");
-	    nuevoNodo = malloc(sizeof(nodo));
-	    nuevoNodo->indice = index;
-	    nuevaFecha.anio = atoi(linea);
-	}
-	if(pos == 1)
-	{
-	    printf("Pos 1\n");
-	    nuevaFecha.mes = atoi(linea);
-	}
-	if(pos == 2)
-	{
-	    printf("Pos 2\n");
-	    nuevaFecha.dia = atoi(linea);
-	}
-	if(pos == 3)
-	{
-	    printf("Pos 3\n");
-	    nuevoNodo->venta= atof("14");
-	    strtok(linea,"$");
-	    printf("%s",strtok(linea,"$"));
-	    nuevoNodo->cuando = nuevaFecha;
-	    index++;
-	}
-	pos++;
-	   
-	    
+		printf("BOI\n");
+		if(atoi(linea) != 0)
+		{
+			if(pos == 0)
+			{
+				printf("Pos 0 %d\n", atoi(linea));
+				nuevoNodo = malloc(sizeof(nodo));
+				nuevoNodo->indice = index;
+				nuevaFecha.anio = atoi(linea);
+			}
+			if(pos == 1)
+			{
+				printf("Pos 1 %d\n",atoi(linea));
+				nuevaFecha.mes = atoi(linea);
+			}
+			if(pos == 2)
+			{
+				printf("Pos 2 %d\n",atoi(linea));
+				nuevaFecha.dia = atoi(linea);
+			}
+			if(pos == 3)
+			{
+				nuevoNodo->venta= atof(linea);
+				printf("Pos 3 %f\n",nuevoNodo->venta);
+				nuevoNodo->cuando = nuevaFecha;
+				index++;
+			}
+			pos++;
+			if(pos == 4)
+			{
+				pos = 0;	
+			}
+		}
+		
+			
+		printf("eoi\n");	
     }
+	printf("Llegue");
     print_lista();
 	
 }
@@ -98,13 +104,23 @@ void print_mes(int val)
 //aniade un nodo al final de la lista
 void add_lista(nodo *p)
 {
-    nodo *aux = prim;
-    while(aux->sig != NULL)
-    {
-	aux=aux->sig;
-    }
-    p->sig = NULL;
-    aux->sig = p;
+	if(prim == NULL)
+	{
+		printf("Prim == NULL\n");
+		prim = p;
+	}
+	else
+	{
+		nodo *aux = prim;
+		while(aux->sig != NULL)
+		{
+		aux=aux->sig;
+		}
+		p->sig = NULL;
+		aux->sig = p;
+	}
+	
+    
 }
 
 void print_fecha(fecha f)
